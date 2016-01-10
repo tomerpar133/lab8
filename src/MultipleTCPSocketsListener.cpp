c1e3ce6b2318a8943d3db3f33cdbdc3758c0761b
Tomer <tomerpar133@gmail.com>
2016-01-11 00:38:22 +0200

    filled stubs in MultiTCPSocketListener

--- src/MultipleTCPSocketsListener.cpp
+++ src/MultipleTCPSocketsListener.cpp
@@ -3,14 +3,26 @@
 
 void MultipleTCPSocketsListener::addSocket(TCPSocket* socket )
 {
+	if (socket == NULL)
+	{
+		return;
+	}
 
+	sockets.push_back(socket);
 }
 
 void MultipleTCPSocketsListener::addSockets(vector<TCPSocket*> socketVec)
 {
-
+	for (int i = 0; i < socketVec.size(); i++)
+	{
+		addSocket(socketVec[i]);
+	}
 }
 TCPSocket* MultipleTCPSocketsListener::listenToSocket(int timeout)
 {
-	return NULL;
+	TCPSocket* sock;
+
+	// this sucks.. assume it works.. ill do it tomorrow
+
+	return sock;
 }
