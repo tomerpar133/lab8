b13e9ac0fe93f6f31d1e6131e923ed96f85354ef
Tomer <tomerpar133@gmail.com>
2016-01-11 18:19:02 +0200

    updated the multiTcp listenAndSelect fucntion

--- Debug/src/MultipleTCPSocketsListener.d
+++
@@ -1,6 +0,0 @@
-src/MultipleTCPSocketsListener.o: ../src/MultipleTCPSocketsListener.cpp \
- ../src/MultipleTCPSocketsListener.h ../src/TCPSocket.h
-
-../src/MultipleTCPSocketsListener.h:
-
-../src/TCPSocket.h:
--- src/MultipleTCPSocketsListener.cpp
+++ src/MultipleTCPSocketsListener.cpp
@@ -1,28 +1,68 @@
 #include "MultipleTCPSocketsListener.h"
-#include "TCPSocket.h"
 
+/*
+ * Add the given socket to the socket list to be listen on
+ */
 void MultipleTCPSocketsListener::addSocket(TCPSocket* socket )
 {
+	//TODO: add he given socket to the socket list
 	if (socket == NULL)
 	{
 		return;
 	}
 
-	sockets.push_back(socket);
+	this->sockets.push_back(socket);
 }
 
+/*
+ * Set the given sockets to the socket list to be listen on
+ */
 void MultipleTCPSocketsListener::addSockets(vector<TCPSocket*> socketVec)
 {
-	for (int i = 0; i < socketVec.size(); i++)
+	//TODO: set the given sockets as the socket list to selct from
+	for (unsigned int i = 0; i < socketVec.size(); i++)
 	{
 		addSocket(socketVec[i]);
 	}
 }
-TCPSocket* MultipleTCPSocketsListener::listenToSocket(int timeout)
+TCPSocket* MultipleTCPSocketsListener::listenToSocket()
 {
-	TCPSocket* sock;
+	//TODO: create local set for the select function (fd_set)
+	fd_set readFd;
+	FD_ZERO(&readFd);
 
-	// this sucks.. assume it works.. ill do it tomorrow
 
-	return sock;
+	//TODO: fill the set with file descriptors from the socket list using (FD_SET macro)
+	int biggestSocket = findBiggestFd(readFd);
+
+	//TODO: perform the select
+	int numOfActiveFds = select(biggestSocket + 1, &readFd, NULL, NULL, NULL);
+
+	//TODO: check the returned value from the select to find the socket that is ready
+	if (numOfActiveFds > 0)
+	{
+		return NULL;
+	}
+
+	//TODO: if select return a valid socket return the matching TCPSocket object otherwise return NULL
+
 }
+
+int MultipleTCPSocketsListener::findBiggestFd(fd_set readFd)
+{
+	int biggest = 0;
+
+	for (unsigned int i = 0; i < this->sockets.size(); i++)
+	{
+		int currentFd = this->sockets[i]->getSocketFd();
+		FD_SET(currentFd, &readFd);
+
+		if (currentFd > biggest)
+		{
+			biggest = currentFd;
+		}
+	}
+
+	return biggest;
+}
+
